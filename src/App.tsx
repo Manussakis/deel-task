96e10ccb4874d49299c2a067f96641fbb891efc8
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2022-08-03 19:03:07 -0300

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
+import { Container } from './components/Container';
+import { AutoComplete } from './components/AutoComplete';
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
+      <header>
+        <Container>
+          <h1>Deel Frontend Test</h1>
+        </Container>
+      </header>
+      <main>
+        <Container>
+          <h2>Search articles</h2>
+          <AutoComplete />
+        </Container>
+      </main>
     </div>
   );
 }
